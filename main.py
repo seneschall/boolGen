95d1b9e9bf603c0927cb020a325d468fca65dcdc
Simon Sack <simon.c.sack@gmail.com>
2022-12-31 18:11:51 +0100

    Add styles and improved maintainability.

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ DNF mit einer spezifizierten Anzahl an Variablen.
 """
 
 
-OPTIONS_LIST = ["-k", "--knf", "-d", "--dnf", "-h", "--help"]
+KNF_FLAGS = ["-k", "--knf"]
+DNF_FLAGS = ["-d", "--dnf"]
+MODES_LIST = [*KNF_FLAGS, *DNF_FLAGS]
+HELP_LIST = ["-h", "--help"]
+STYLE_FLAG_LIST = ["-s", "--style"]
+STYLES_LIST = ["l", "latex", "p", "plain-text"]
+ALL_FLAGS = [*MODES_LIST, *HELP_LIST, *STYLE_FLAG_LIST, *STYLES_LIST]
 
 HELP_TEXT = f"""
 boolGen kann mit den folgenden Parametern aufgerufen werden:
 
-main.py {OPTIONS_LIST!r} [Anzahl der Variablen (zwischen 1 und 9)]
+main.py {MODES_LIST!r} [Anzahl der Variablen (zwischen 1 und 9)]
 
 Die flags geben an, ob eine Formel in DNF oder in KNF generiert wird.
 Die Anzahl am Ende gibt an, wie viele Variablen die Formel enthält.
@@ -27,22 +33,57 @@ def help_text():
     exit()
 
 
-options = sys.argv[1]
-
-if options == "-h" or options == "--help":
+def error_text():
+    print("ERROR: Eingabe nicht erkannt!")
     help_text()
 
 
-num_of_vars = int(sys.argv[2])
+arguments = sys.argv[1:]
 
-if num_of_vars > 9 or num_of_vars < 1:
+help_true = bool([x for x in HELP_LIST if x in arguments])
+
+if help_true:
     help_text()
 
-if options == "-k" or options == "--knf":
-    print(knf_gen(gen_var_list(num_of_vars)))
+try:
+    NUM_OF_VARS = int(arguments[-1])
+except TypeError:
+    error_text()
 
-elif options == "-d" or options == "--dnf":
-    print(dnf_gen(gen_var_list(num_of_vars)))
+mode = [x for x in arguments if x in MODES_LIST]
+
+mode_is_valid = bool(mode) and len(mode) == 1
+
+if not mode_is_valid:
+    raise SyntaxError(f"Modus wurde nicht erkannt!\n{HELP_TEXT}")
+
+style_flag_list = [x for x in arguments if x in STYLE_FLAG_LIST]
+
+style_is_specified = bool(style_flag_list)
+
+style_flag = style_flag_list[0]
+style_index = arguments.index(style_flag) + 1
+
+if style_is_specified:
+    style: str = arguments[style_index]
+    if len(style) > 1:
+        style = style[:1]  # so that we can treat style as a single letter
+    if style not in STYLES_LIST:
+        raise SyntaxError("Style wurde nicht erkannt.")
+    else:
+        print("Hurra!")
+else:
+    style: str = "l"  # Latex is default
+
+if mode[0] in KNF_FLAGS:
+    print(
+        knf_gen(gen_var_list(NUM_OF_VARS))
+    )  # These functions should get the style as input
+
+elif mode[0] in DNF_FLAGS:
+    print(
+        dnf_gen(gen_var_list(NUM_OF_VARS))
+    )  # These functions should get the style as input
 
 else:
-    help_text()
+    raise SyntaxError(f"Modus wurde nicht erkannt!\n{HELP_TEXT}")
